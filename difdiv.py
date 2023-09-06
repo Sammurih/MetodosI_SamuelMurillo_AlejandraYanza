d0b9e5e411c6d510c210cadfb0d61b0f2328ab79
Ale1639 <a.yanza@uniandes.edu.co>
2023-09-06 17:42:49 -0500

    abc

--- difdiv.py
+++ difdiv.py
@@ -6,3 +6,11 @@ def diferencia_div_3(x0,x1,x2,F):
     resul= (diferencia_div_2(x1,x2,F)- diferencia_div_2(x0,x1,F))/(x2-x0)
     return resul
 
+def define_abc(x0,x1,x2,F):
+    a=diferencia_div_3(x0,x1,x2,F)
+    b=diferencia_div_2(x0,x1,F)-((x0+x1)*(diferencia_div_3(x0,x1,x2,F)))
+    c=F(x0)-((x0)*diferencia_div_2(x0,x1,F))+(x0*x1*diferencia_div_3(x0,x1,x2,F))
+
+    return [a,b,c]
+
+    
