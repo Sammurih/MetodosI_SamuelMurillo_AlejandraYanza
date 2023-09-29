644cd7aad32e4bfc320d9208d5645ba6cae3f017
Sammurih <s.murilloh2@uniandes.edu.co>
2023-09-29 16:36:38 -0500

    Punto 6

--- Parcial2/Ejercicio6.py
+++ Parcial2/Ejercicio6.py
@@ -1,11 +1,48 @@
 import numpy as np
 import sympy as sym
 import math
-
-a=0.01
 R=0.5
-f = lambda x: (np.sqrt(a**2+x**2))/(R+x)
+a=0.01
+n=10000
+x = np.linspace(-a,a,n)
+IntegralTeorica=np.pi*(R-np.sqrt(R**2-a**2))
 
-x = sym.Symbol('x',real=True)
-y = sym.Symbol('y',real=True)
-n=6
+print(x)
+def Function(x):
+    a=0.01
+    R=0.5
+    return (np.sqrt(a**2-x**2))/(R+x)
+
+y = Function(x)
+
+def IntegrateTrapecio(n,x,y):
+    h=x[1]-x[0]
+    sum=0
+    for i in range(0,n):
+        if i==0:
+            sum+=y[i]
+        elif i==n-1:
+            sum+=y[i]
+        else:
+            sum+=2*y[i]
+    return (h/2)*sum
+
+def Error(Teorico,Experimental):
+    return 100*(1-np.abs(Experimental)/np.abs(Teorico))
+IntegralTrapecio=IntegrateTrapecio(n,x,y)
+print(IntegralTrapecio)
+print(IntegralTeorica) 
+print(Error(IntegralTeorica,IntegralTrapecio))
+
+def Simpson(x,y):
+    h=x[1]-x[0]
+    x1=x[0]
+    for i in range(1,len(x)):
+        x2=x[i]
+        xm=(x2+x1)/2
+        suma=(h/3)*(y(x1)+4*y(xm)+y(x2))
+        x1=x2
+    return suma
+
+IntegralSimpson=Simpson(x,Function)
+print(IntegralSimpson)
