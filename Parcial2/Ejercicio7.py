a0107a95cd07fc3c5692225973186316351637b6
Sammurih <s.murilloh2@uniandes.edu.co>
2023-09-29 18:45:22 -0500

    Punto 7

--- Parcial2/Ejercicio7.py
+++ Parcial2/Ejercicio7.py
@@ -1,13 +1,21 @@
 import numpy as np
 
-def volumsemiesfera(r):
-    f=(4/3)*np.pi*r**3
-    return r/2
+def integrate(n):
+    m=np.zeros((n+1,n+1))
+    for i in range(n+1):
+        for j in range(n+1):
+            val=1-(i*2/n2)-(j2/n*2)
+            if val>=0:
+                m[i][j]=np.sqrt(val)
 
+    xis=len(m)
+    yis=len(m[0])            
+    v = 0.0
+    for i in range(xis-1):
+        for j in range(yis-1):
+            avg_value = (m[i][j]) + m[i+1][j]+m[i][j+1]+m[i+1][j+1]
+            
+            
+            v += avg_value/n**2
 
-
-
-
-
-
-
+    return v
