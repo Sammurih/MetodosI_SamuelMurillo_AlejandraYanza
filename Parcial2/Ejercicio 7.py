7d222afab791bad61e061c7ab015253ee32dd284
Ale1639 <a.yanza@uniandes.edu.co>
2023-09-29 16:54:50 -0500

    7

---
+++ Parcial2/Ejercicio 7.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def volumsemiesfera(r):
+    f=(4/3)*np.pi*r**3
+    return r/2
+
+
+def cuadrillaxy (R):
+    cua=np.linspace(-R,R)
+    return cua 
+print(cuadrillaxy(1))
+
+
+
+
+
