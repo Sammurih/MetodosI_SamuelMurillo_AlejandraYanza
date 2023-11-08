78c7ef3a1f11f0b2d7f26fc501612ce18b8b54cd
Sammurih <s.murilloh2@uniandes.edu.co>
2023-11-07 23:02:02 -0500

    Preparcial

--- Parcial3/Ejercicio21.py
+++ Parcial3/Ejercicio21.py
@@ -1,13 +1,15 @@
 import numpy as np
 import sympy as sym
+
 x = sym.Symbol('x',real=True)
 y = sym.Symbol('y',real=True)
+
 def f(x,y):
     k=8.98755e9
     q=3e-4
     l=5
     w=114.6
-    c=((k**2)*(q**4)*(3+6*sym.sqrt(2))**2)/((w**2)*(4**2)*(l**4))
+    c=((k**2)*(q**4)*(1+2*sym.sqrt(2))**2)/((w**2)*(4**2)*(l**4)) #Aprox 0.07301955665405001
     z = x + sym.I*y
     f = (sym.sin(z))**6+c*(sym.sin(z))**2-c
     f = f.expand()
@@ -16,7 +18,8 @@ def f(x,y):
 
 f0,f1 = f(x,y)
 F = [f0,f1]
-print(F)
+
+
 J = sym.zeros(2,2)
 
 for i in range(2):
@@ -26,15 +29,14 @@ for i in range(2):
         else:
             J[i,j] = sym.diff(F[i],y,1)
 
-#print(J)
+
 InvJ = J.inv(method='LU')
-#print(InvJ)
 
 Fn = sym.lambdify([x,y],F,'numpy')
 IJn = sym.lambdify([x,y],InvJ,'numpy')
-#print(IJn(1.,1.))
 
-def NewtonRaphson(z,Fn,Jn,itmax=100,precision=1e-9):
+
+def NewtonRaphson(z,Fn,Jn,itmax=1000,precision=1e-10):
     
     error = 1
     it = 0
@@ -53,17 +55,40 @@ def NewtonRaphson(z,Fn,Jn,itmax=100,precision=1e-9):
         
     print(it)
     return z
-z0 = np.array([1,-2])
-zsol = NewtonRaphson(z0,Fn,IJn)
-print(zsol)
 
-x,y=zsol
-k=8.98755e9
-q=3e-4
-l=5
-w=114.6
-c=((k**2)*(q**4)*(3+6*2**(1/2))**2)/((w**2)*(4**2)*(l**4))
-z = x + sym.I*y
-f = (sym.sin(z))**6+c*(sym.sin(z))**2-c
-
-print(sym.simplify(f))
+z0 = np.array([-0.5,0 ])
+z1 = np.array([0.5,0])
+z2 = np.array([2,0])
+z3 = np.array([4,0])
+z4 = np.array([-0.5,-0.5])
+z5 = np.array([-0.5,0.5])
+z6 = np.array([0.5,-0.5])
+z7 = np.array([0.5,0.5])
+z8 = np.array([2.81,-0.56])
+z9 = np.array([2.81,0.56])
+z10 = np.array([3,-0.5])
+z11 = np.array([3,0.5])
+zsol0 = NewtonRaphson(z0,Fn,IJn)
+print(zsol0)
+zsol1 = NewtonRaphson(z1,Fn,IJn)
+print(zsol1)
+zsol2 = NewtonRaphson(z2,Fn,IJn)
+print(zsol2)
+zsol3 = NewtonRaphson(z3,Fn,IJn)
+print(zsol3)
+zsol4 = NewtonRaphson(z4,Fn,IJn)
+print(zsol4)
+zsol5 = NewtonRaphson(z5,Fn,IJn)
+print(zsol5)
+zsol6 = NewtonRaphson(z6,Fn,IJn)
+print(zsol6)
+zsol7 = NewtonRaphson(z7,Fn,IJn)
+print(zsol7)
+zsol8 = NewtonRaphson(z8,Fn,IJn)
+print(zsol8)
+zsol9 = NewtonRaphson(z9,Fn,IJn)
+print(zsol9)
+zsol10 = NewtonRaphson(z10,Fn,IJn)
+print(zsol10)
+zsol11 = NewtonRaphson(z11,Fn,IJn)
+print(zsol11)
