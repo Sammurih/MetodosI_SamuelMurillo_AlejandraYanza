64d0a881b82306b4d5e698d40dca0ffb3bdccac4
Sammurih <s.murilloh2@uniandes.edu.co>
2023-11-30 18:10:43 -0500

    Mínimos Cuadrados 1b

--- Parcial4/Minimos_Cuadrados_1b.py
+++ Parcial4/Minimos_Cuadrados_1b.py
@@ -1,12 +1,17 @@
 import numpy as np
 import matplotlib.pyplot as plt
-
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
 x = np.linspace(-5,5,100)
 y = np.zeros((3,len(x)))
 
 y[0] = 2*x - 2.
 y[1] = 0.5 - 0.5*x
 y[2] = 4 - x
+
+f1 =lambda _x,_y: 2*_x -_y - 2.
+f2 =lambda _x,_y: _x + 2*_y - 1.
+f3 =lambda _x,_y: _x +_y - 4.
 
 A= np.array([[2,-1],
              [1,2],
@@ -35,18 +40,18 @@ print(np.linalg.norm(np.dot(A,xsol)-b))
 
 #AHORA HACIENDO UNA BUSQUEDA ITERATIVA
 
-def IterativeMinimize(A,b,h=0.04):
+def IterativeMinimize(A,b,h=0.05):
     resultados=np.array([])
     puntos=np.array([[0,0]])
-    x_= np.arange(-5,5+h,h)
-    y_= np.arange(-5,5+h,h)
+    x_= np.arange(-5,5,h)
+    y_= np.arange(-5,5,h)
     for i in x_:
         for j in y_:
-            resultados=np.append(resultados,np.linalg.norm(np.dot(A,[i,j])-b))
+            resultados=np.append(resultados,np.linalg.norm(np.matmul(A,[i,j])-b))
             punto=(i,j)
             puntos=np.append(puntos,[punto],axis=0)
             print(resultados[-1])
-            #print(puntos[-1])
+            print(puntos[-1])
     resultados1=np.sort(resultados)
     #print(np.where(resultados==resultados1[0]))
     #print(np.where(resultados==resultados1[0])[0])
@@ -59,6 +64,27 @@ for l in range(y.shape[0]):
     plt.plot(x,y[l],ls='--',lw=2)
 plt.scatter(xsol[0],xsol[1],color='r')
 plt.scatter(P[0][0],P[0][1],color='b')
+plt.show()
+
+# Generar datos
+x = np.arange(-5, 5, 0.01)
+y = np.arange(-5, 5, 0.01)
+x, y = np.meshgrid(x, y)
+z = np.sqrt(f1(x,y)**2+f2(x,y)**2+f3(x,y)**2)
+
+# Crear la figura y los ejes 3D
+fig = plt.figure()
+ax = fig.add_subplot(111, projection='3d')
+
+# Plot 3D
+ax.plot_surface(x, y, z, cmap='viridis')
+
+# Configurar etiquetas
+ax.set_xlabel('X Label')
+ax.set_ylabel('Y Label')
+ax.set_zlabel('Z Label')
+
+# Mostrar el gráfico
 plt.show()
 
 #ITERATIVAMENTE ES MUCHO MÁS PESADO COMPUTACIONALMENTE Y NO ES TAN PRECISO COMO EL MÉTODO DE MÍNIMOS CUADRADOS
